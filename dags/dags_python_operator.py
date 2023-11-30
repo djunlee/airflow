1b718546f09ed5a8e2122a17b35c95b4a55288d0
djlee <dj961024@gmail.com>
2023-12-01 00:05:47 +0900

    python operator 추가

---
+++ dags/dags_python_operator.py
@@ -0,0 +1,11 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.email import EmailOperator 
+
+with DAG(
+    dag_id="dags_email_operator",
+    schedule="0 8 1 * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
