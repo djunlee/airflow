adeab8129763f6626bc05dee26ac1fbc172c04ff
djlee <dj961024@gmail.com>
2023-12-02 00:25:48 +0900

    task_decorator 업로드

--- dags/dags_python_task_decorator.py
+++ dags/dags_python_task_decorator.py
@@ -1,10 +1,16 @@
 from airflow import DAG
 import pendulum
+from airflow.decorators import task
+
 
 with DAG(
-    dag_id="example_python_operator",
-    schedule=None,
-    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    dag_id="dags_python_task_decorator",
+    schedule="0 2 * * 1",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False,
-    tags=["example"],
 ) as dag:
+    @task(task_id="python_task_1")
+    def print_context(some_input):
+        print(some_input)
+
+    python_task_1 = print_context('task_decorator 실행')
