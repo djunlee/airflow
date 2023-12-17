44089201d02e965ee8abbe9c4a21435e4028d9b2
djlee <dj961024@gmail.com>
2023-12-17 23:44:54 +0900

    task group dag 수정

--- dags/dags_python_with_task_group.py
+++ dags/dags_python_with_task_group.py
@@ -32,7 +32,7 @@ with DAG(
 
         inner_func1() >> inner_function2
 
-    with TaskGroup(group_id='second_group', toolpit='두 번째 그룹입니다.') as group_2:
+    with TaskGroup(group_id='second_group', tooltip='두 번째 그룹입니다.') as group_2:
         ''' 여기에 적은 docstring은 표시되지 않습니다.'''
         @task(task_id='inner_function1')
         def inner_func1(**kwags):
