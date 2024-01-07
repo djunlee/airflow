3359d4340a69d94b5e58c034eb2bfbaa0fef91f0
djlee <dj961024@gmail.com>
2024-01-07 23:43:30 +0900

    db insrt dag 수정

--- dags/dags_python_with_postgres.py
+++ dags/dags_python_with_postgres.py
@@ -26,7 +26,7 @@ with DAG(
     insrt_postgres = PythonOperator(
         task_id='insrt_postgres',
         python_callable=insrt_postgres,
-        op_args=['172.28.0.3', '5430', 'djlee', 'djlee', 'djlee']
+        op_args=['172.28.0.3', '5432', 'djlee', 'djlee', 'djlee']
     )
 
     insrt_postgres
